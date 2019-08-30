cfa14bc4751512ec10bbfac21ba225e4d5492ed2
wepplorg <amberyadav@gmail.com>
2019-08-30 15:27:06 +0530

    secure urls

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -1,7 +1,7 @@
 <title>weppl</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="shortcut icon" type="image/png" href="{{asset('img/icons/fevicon.png')}}" />
+<link rel="shortcut icon" type="image/png" href="{{secure_asset('img/icons/fevicon.png')}}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
@@ -11,10 +11,10 @@
 
 <!-- new design custom css and js -->
   <!-- Icons-->
-    <link href="{{ asset('assets/@coreui/icons/css/coreui-icons.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('assets/flag-icon-css/css/flag-icon.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('assets/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
-    <link href="{{ asset('assets/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
+    <link href="{{ secure_url('assets/@coreui/icons/css/coreui-icons.min.css')}}" rel="stylesheet">
+    <link href="{{ secure_url('assets/flag-icon-css/css/flag-icon.min.css')}}" rel="stylesheet">
+    <link href="{{ secure_url('assets/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{ secure_url('assets/simple-line-icons/css/simple-line-icons.css')}}" rel="stylesheet">
     <!-- Main styles for this application-->
     
 
@@ -32,18 +32,18 @@
      <!-- CoreUI and necessary plugins-->
     <script src="{{secure_url('assets/jquery/dist/jquery.min.js')}}"></script>
    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>-->
-    <script src="{{asset('assets/popper.js/dist/umd/popper.min.js')}}"></script>
-    <script src="{{asset('assets/bootstrap/dist/js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('assets/pace-progress/pace.min.js')}}"></script>
-    <script src="{{asset('assets/perfect-scrollbar/dist/perfect-scrollbar.min.js')}}"></script>
-    <script src="{{ asset('assets/@coreui/coreui/dist/js/coreui.min.js')}}"></script>
+    <script src="{{secure_url('assets/popper.js/dist/umd/popper.min.js')}}"></script>
+    <script src="{{secure_url('assets/bootstrap/dist/js/bootstrap.min.js')}}"></script>
+    <script src="{{secure_url('assets/pace-progress/pace.min.js')}}"></script>
+    <script src="{{secure_url('assets/perfect-scrollbar/dist/perfect-scrollbar.min.js')}}"></script>
+    <script src="{{ secure_url('assets/@coreui/coreui/dist/js/coreui.min.js')}}"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote-bs4.js"></script>
      <!-- datatables -->
     <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
     
-    <script src="{{ asset('js/share.js') }}"></script>
-    <script src="{{asset('js/sticky.js')}}"></script>
+    <script src="{{ secure_url('js/share.js') }}"></script>
+    <script src="{{secure_url('js/sticky.js')}}"></script>
     <script type="text/javascript" src="//script.crazyegg.com/pages/scripts/0086/2507.js" async="async"></script>
     <script>
     $(document).ready(function() {
